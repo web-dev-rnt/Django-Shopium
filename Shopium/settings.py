4b43dfc5819d463a627d4ba10043333535e9d335
Rudra Narayan Tiwari <webdevrnt@gmail.com>
2026-06-22 03:40:02 +0530

    Update settings.py

--- Shopium/settings.py
+++ Shopium/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', default=False, cast=bool)
 
-ALLOWED_HOSTS = '*'
+ALLOWED_HOSTS = ['*']
 
 # CSRF trusted origins for Railway (and any custom domain)
 CSRF_TRUSTED_ORIGINS = config(
